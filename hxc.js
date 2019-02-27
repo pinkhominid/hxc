8f0525e2c3a718175d442849a000413852e6f2fd
John Ericson <pinkhominid@birdbomb.com>
2019-02-26 23:13:23 -0500

    border-box for all components

--- hxc.js
+++ hxc.js
@@ -1,3 +1,9 @@
+/**
+ * H×C
+ * Copyright (c) pinkhominid <pinkhominid@birdbomb.com>
+ * MIT license
+ */
+
 export * from './lib/hxc-drawer.js';
 export * from './lib/hxc-icon.js';
 export * from './lib/hxc-spinner.js';
